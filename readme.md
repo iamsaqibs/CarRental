e69bc40197872f102f2b2267a509db1040394c60
Saqib S <msaqibsaeed4@gmail.com>
2020-07-30 20:33:32 +0500

    Final Commit

--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 # Car sales and e-commerce app
+
+## composer install
+## cp .env.example .env
+## Set up DB in .env
+## php artisan migrate
+## php artisan serve
--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -1,10 +1,10 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-3">
-			<p class="format-fa"><i class="format-fa fa fa-envelope"></i>office@wcauto.com</p>
+			<p class="format-fa"><i class="format-fa fa fa-envelope"></i>info@example.com</p>
 		</div>
 		<div class="col-md-3">
-			<p class="format-fa"><i class="format-fa fa fa-phone"></i>219-221-8702</p>
+			<p class="format-fa"><i class="format-fa fa fa-phone"></i>051-5124236</p>
 		</div>
 		<div class="col-md-3"></div>
 		<div class="col-md-3"></div>
